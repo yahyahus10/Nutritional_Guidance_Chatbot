a68343ac858c411b699a949c4e0a73733161fedd
DESKTOP-FAR85NI\user <mohammad.dowlut5@umail.uom.ac.mu>
2024-04-24 18:21:37 +0400

    seven

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template, jsonify, request
 from src.helper_ import download_hugging_face_embeddings
 from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
+from langchain_community.llms import CTransformers
 from langchain.chains import RetrievalQA
 from langchain_chroma import Chroma
 from langchain_community.embeddings.sentence_transformer import (
     SentenceTransformerEmbeddings,)
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from src.prompt_ import*
+from src.helper_ import load_pdf, load_web, text_split, download_hugging_face_embeddings
+from langchain_chroma import Chroma
+
 import os
 
 app=Flask(__name__)
 
+Hypertension_document_pdf=load_pdf("Hypertension_data/")
+Hypertension_document_web_1 = load_web("https://www.webmd.com/hypertension-high-blood-pressure/high-blood-pressure-diet")
+Hypertension_document_web_2=load_web("https://www.nhs.uk/live-well/eat-well/food-guidelines-and-food-labels/the-eatwell-guide/")
+
+extracted_merged_Hpertension_data = Hypertension_document_web_1 + Hypertension_document_pdf + Hypertension_document_web_2
+text_chunks=text_split(extracted_merged_Hpertension_data)
+
 embeddings=download_hugging_face_embeddings()
+
+db = Chroma.from_documents(text_chunks, embeddings, persist_directory="./chroma_db")
 
 PROMPT=PromptTemplate(template=promt_template, input_variables=["context", "question"])
 
@@ -44,4 +56,16 @@ qa = RetrievalQA.from_chain_type( #reate an instance of a RetrievalQA class
 
 @app.route("/")
 def index():
-    return_template
+    return render_template('chat.html')
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
